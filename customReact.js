c7bd201f2f437eaa4502b5bf47ea2ffe02205b4e
msk-saad <developersaad12@gmail.com>
2024-10-16 09:54:14 +0530

    custom render of react using html and js

--- customReact.js
+++ customReact.js
@@ -1,2 +1,20 @@
+function customRender (element, container) {
+
+  const domElement = document.createElement(element.type);
+  domElement.innerHTML = element.children;
+  domElement.setAttribute('href', element.props.href);
+  domElement.setAttribute('target', element.props.target);
+
+  container.appendChild(domElement);
+     
+const reactElement = {
+  type: 'a',
+  props: {
+    href: 'https://www.google.com',
+    target: '_blank'
+  },
+  children: 'Click to visit google'
+}
+
 const mainContainer = document.querySelector('#root');
-mainContainer.innerHTML = "Hello World!";
+customRender(reactElement, mainContainer)
