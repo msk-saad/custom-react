ca8b9191a6c1b6e319b429c6255bc3f7dd1edfcf
msk-saad <developersaad12@gmail.com>
2024-10-16 09:55:26 +0530

    modular custom rendering v2.

--- customReact.js
+++ customReact.js
@@ -1,12 +1,22 @@
 function customRender (element, container) {
-
+  /* The issue with this code is it is not modular in setting attributes
   const domElement = document.createElement(element.type);
   domElement.innerHTML = element.children;
   domElement.setAttribute('href', element.props.href);
   domElement.setAttribute('target', element.props.target);
 
   container.appendChild(domElement);
-     
+  */ 
+
+  const domElement = document.createElement(element.type);
+  domElement.innerHTML = element.children;
+  for (const prop in element.props) {
+    if (prop === 'children') continue;
+    domElement.setAttribute(prop, element.props[prop])
+  }
+  container.appendChild(domElement);
+}
+
 const reactElement = {
   type: 'a',
   props: {
